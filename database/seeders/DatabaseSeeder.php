55049f82777725f1daadbd317ebf911db7a78d49
RobertoVida <roberto50005000@gmail.com>
2023-11-27 15:27:40 -0300

    CASOS FAKE

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // USUÁRIOS
         // \App\Models\User::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        
+
+        // CASOS
+        // for($i = 0; $i < 50; $i++)
+        // {
+        //     $situacao_caso = ["aberto", "resolvido", "fracassado", "arquivado"];
+        //     $prioridade_caso = ["baixa", "media", "alta"];
+        //     DB::table("casos")->insert([
+        //         "numero_caso" => rand(10237, 98436)."-".uniqid()."/"
+        //         .date("d")."_".date("m")."_".date("Y"),
+        //         "titulo_caso" => fake()->text(15),
+        //         "prioridade_caso" => $prioridade_caso[rand(0, 2)],
+        //         "situacao_caso" => $situacao_caso[rand(0, 3)],
+        //         "detalhes_caso" => fake()->text(1200),
+        //         "ids_investigador" => "",
+        //         "created_at" => now(),
+        //         "updated_at" => now()
+        //     ]);
+        // }
     }
 }
