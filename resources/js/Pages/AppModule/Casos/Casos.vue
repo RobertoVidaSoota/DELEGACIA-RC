be9120ab3a03a2fdd2579021ae689cd1a8dcf430
RobertoVida <roberto50005000@gmail.com>
2023-11-27 15:50:30 -0300

    RECUPERAR DADOS NA TABELA OK

--- resources/js/Pages/AppModule/Casos/Casos.vue
+++ resources/js/Pages/AppModule/Casos/Casos.vue
@@ -5,6 +5,7 @@
 
     <main>
         <h1>Casos</h1>
+        <Table :tableHead="tHeads" :dataTable="data"/>
         <Footer />
     </main>
 </template>
@@ -16,6 +17,20 @@ import Footer from "../../Components/Footer.vue"
 import Table from "../../Main/Table.vue";
 import ButtonAdd from "../../Main/ButtonAdd.vue";
 import FormCasos from "./FormCasos.vue";
+
+const tHeads = ["Número", "Título", "Prioridade", "Data"]
+const data = [
+    {
+        "Número": "1", "Título": "1 caso", "Prioridade": "Média", "Data": "01/01/2020"
+    },
+    {
+        "Número": "2", "Título": "2 caso", "Prioridade": "Alta", "Data": "01/01/2020"
+    },
+    {
+        "Número": "3", "Título": "3 caso", "Prioridade": "Baixa", "Data": "01/01/2020"
+    },
+]
+
 </script>
 
 <style>
